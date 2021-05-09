5327b64d45f41a1b62e47202a1366a12188e7a33
RoshRoslin <RoshRoslin@gmail.com>
2021-05-09 16:22:39 -0700

    Updates

---
+++ precache-manifest.92d981646e827229eb88e729d4006253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315ffb0c09a4c420632491e0daf2126c",
+    "url": "/react-color-generator-project/index.html"
+  },
+  {
+    "revision": "f6566d1cd8d2f9d6f39e",
+    "url": "/react-color-generator-project/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/react-color-generator-project/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/react-color-generator-project/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6566d1cd8d2f9d6f39e",
+    "url": "/react-color-generator-project/static/js/main.e0869c81.chunk.js"
+  },
+  {
+    "revision": "0eefd4d5d67201e2270f",
+    "url": "/react-color-generator-project/static/js/runtime-main.646af7d0.js"
+  }
+]);
